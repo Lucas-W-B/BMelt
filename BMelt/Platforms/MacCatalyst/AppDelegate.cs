62cab17df1ac89f743f09795b1e4b202773dad77
Lucas <diredarknil@gmail.com>
2022-04-07 13:22:31 -0700

    Add project files.

---
+++ BMelt/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace BMelt;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
