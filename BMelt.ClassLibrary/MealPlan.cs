a2cbfe0e5a569e1cf6c4e2ba5f233a72e4bcc59a
Lucas-W-B <lucaswbaum@gmail.com>
2022-04-08 11:09:44 -0700

    Recipes view with css

---
+++ BMelt.ClassLibrary/MealPlan.cs
@@ -0,0 +1,8 @@
+﻿namespace BMelt.ClassLibrary
+{
+    public class MealPlan
+    {
+        public SortedDictionary<short, Recipe> Recipes { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
